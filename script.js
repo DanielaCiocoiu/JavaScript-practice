22309e7319bdef95907da10ed392eb4463eb4ec8
shantiox <danaciocoiu78@gmail.com>
2018-12-05 20:04:16 +0200

    index

---
+++ script.js
@@ -0,0 +1,24 @@
+4*3;
+if (4*3==7) {
+	alert("You're smart!");
+}
+
+
+function sayHello(){
+	console.log("Hello");
+}
+
+sayHello();
+
+function sing(){
+	console.log("AAAAAA");
+	console.log("bbbbbbb")
+}
+sing();
+
+function sing(song){
+	console.log(song);
+
+}
+sing("aaaaa");
+sing("bbbbb");
