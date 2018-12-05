65fe9ebbc8d70d49570f10c39dbd91fff7df51ad
shantiox <danaciocoiu78@gmail.com>
2018-12-05 20:19:22 +0200

    Update Learn JavaScript.js

--- Learn JavaScript.js
+++ Learn JavaScript.js
@@ -80,3 +80,26 @@ function checkDriverAge() {
 }
 checkDriverAge();
 
+//2. Function Expression.
+var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+checkDriverAge2();
+// folosesc console.log si fac functia checkDriverAge() sa accepte orice argument
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		return "Sorry, you are too yound to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
+console.log(checkDriverAge(92));
